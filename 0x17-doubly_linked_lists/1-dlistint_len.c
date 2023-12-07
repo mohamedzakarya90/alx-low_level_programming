cee3ee5ec009435ff401d7e7eb5771a6ddf37c44
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2023-12-07 00:34:09 -0800

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ **/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t cont = 0;
+
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
+}
