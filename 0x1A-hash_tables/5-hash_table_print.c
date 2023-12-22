d045e0522dc478b37f237c9cda51a1ac4ef64064
mohamed zakarya mohamed farag <mohamed.zakrya.farag@gmail.com>
2023-12-21 21:36:25 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_print : Prints a hash table.
  * @ht: A pointer to the hash table to print.
- * mohamed zakarya farag
+ * author : mohamed zakarya farag
  * Description: Key/value pairs are printed in the order
  *              they appear in the array of the hash table.
  */
